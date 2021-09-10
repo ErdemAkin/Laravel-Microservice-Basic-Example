918aaa7194edc2b887250a99891529e41858331f
Ali Erdem Akın <erdem.akin@aerotek.com.tr>
2021-09-10 21:51:36 +0300

    Initial Commit

---
+++ Main/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+ */
+
+Route::get('product', [
+    ProductController::class,
+    'index',
+]);
+
+Route::post('products/{id}/like', [
+    ProductController::class,
+    'like'
+]);
